248453ac7c90f3046d25491bc6dd851ff7a2c0f3
Louis Ruocco <louisruocco1@gmail.com>
2023-03-10 09:44:10 +0000

    created basic commands

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+
+$shell = $Host.UI.RawUI
+$shell.ForegroundColor = "green"
+
+set-location C:\
+function openmail {
+    start-process ms-unistore-email:
+}
+
+
+function p {
+    param(
+        [Parameter(Mandatory)]
+        [string]$address
+    )
+
+    $ping = test-connection $address -count 1 -quiet
+    if($ping -eq "true"){
+        return write-host "$address online"
+    } else {
+        return write-host "$address offline"
+    }
+}
