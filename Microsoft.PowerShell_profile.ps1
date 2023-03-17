219aedcb9863147f25a3b2ca5092ae9f3f904309
Louis Ruocco <louisruocco1@gmail.com>
2023-03-17 13:48:01 +0000

    .

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,21 +4,14 @@ $shell.ForegroundColor = "green"
 
 set-location C:\
 
-<<<<<<< HEAD
 $date = date
 
 write-host $date
-<<<<<<< HEAD
-=======
 
-=======
 Start-Process -FilePath "powershell" -Verb RunAs
->>>>>>> develop
->>>>>>> parent of 64ffc86 (.)
 function openmail {
     start-process ms-unistore-email:
 }
-
 
 function p {
     param(
