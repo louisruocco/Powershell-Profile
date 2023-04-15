aac4e73e05273e6de2387594a5f37fd3e728725f
Louis Ruocco <louisruocco1@gmail.com>
2023-04-15 22:48:48 +0100

    reworked psprofileupdate function to be more universal across different devices

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,5 +19,18 @@ function p {
 }
 
 function psprofileupdate {
-    start-process "\Code\Update-PSprofile\script.ps1"
+    $url = "https://github.com/louisruocco/Update-Powershell-Profile-Script"
+
+    New-Item -Path "C:\Documents\updatepsprofile" -Type Directory
+    set-location "C:\Documents\updatepsprofile"
+
+    git init
+    git remote add origin $url
+    git clone $url
+
+    Start-Process "C:\Documents\updatepsprofile\Update-Powershell-Profile-Script\script.ps1"
+
+    Remove-Item C:\Documents\updatepsprofile
+
+    write-host "Profile Updated!"
 }
