c33a6ef89876b24e168f44e2225719d0a340e70f
Louis Ruocco <louisruocco1@gmail.com>
2023-04-17 14:11:12 +0100

    renamed function and added ability to remove path and close terminal down

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,32 @@
+
+$shell = $Host.UI.RawUI
+$shell.ForegroundColor = "green"
+function openmail {
+    start-process ms-unistore-email:
+}
+function p {
+    param(
+        [Parameter(Mandatory)]
+        [string]$address
+    )
+
+    $ping = test-connection $address -count 1 -quiet
+    if($ping -eq "true"){
+        return write-host "$address online"
+    } else {
+        return write-host "$address offline"
+    }
+}
+
+function updateprofile {
+    $url = "https://github.com/louisruocco/Update-Powershell-Profile-Script"
+    $path = new-item -Path "$home\documents\psprofileupdate" -type Directory
+    set-location $path
+    git init
+    git remote add origin $url
+    git clone $url
+    start-process "$home\documents\psprofileupdate\update-powershell-profile-script\script.ps1"
+    set-location C:\
+    remove-item $path -force
+    exit
+}
