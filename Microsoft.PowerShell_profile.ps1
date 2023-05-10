2748f87511224dd5d26b3851901f3480d35d44a1
Louis Ruocco <louisruocco1@gmail.com>
2023-05-10 21:10:41 +0100

    added updateprofile function to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,3 +28,25 @@ function psprofileupdate {
     start-process "$home\documents\psprofileupdate\update-powershell-profile-script\script.ps1"
     remove-item $path -force
 }
+
+function updateprofile {
+    $url = "https://github.com/louisruocco/Powershell-Profile"
+
+    New-Item -Path "C:\psprofile" -Type Directory
+    Set-Location -Path "C:\psprofile"
+
+    git init
+    git remote add origin $url
+    git pull origin master
+
+    robocopy "C:\psprofile" "$home\Documents\WindowsPowerShell" /xj /tee /np /r:0 /w:0
+
+    set-location $home
+
+    Remove-Item -Path "C:\psprofile" -force
+
+    write-host "Profile Updated!"
+    start-sleep 2
+
+    start-process powershell.exe
+}
